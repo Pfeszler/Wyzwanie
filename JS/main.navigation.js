8673bd68ed476db1727f45c76884926f11d02dfd
Pawel.Feszler <pfeszler@gmail.com>
2020-04-29 17:37:28 +0200

    bugg fixes

--- JS/main.navigation.js
+++ JS/main.navigation.js
@@ -1,6 +1,7 @@
 (() => {
     const button = document.querySelector(".js-button");
     const button2 = document.querySelector(".js-button2");
+    const button3 = document.querySelector(".js-button3");
     const menu = document.querySelector(".js-main-navigation__list");
 
     const toggleClass = () => {
@@ -9,5 +10,6 @@
 
     button.addEventListener("click", toggleClass);
     button2.addEventListener("click", toggleClass);
+    button3.addEventListener("click", toggleClass);
 
 })();
